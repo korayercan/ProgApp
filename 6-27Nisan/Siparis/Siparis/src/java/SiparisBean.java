72f528257013c4562f4123a6b75f5cf72675128b
korayercan <75748515+korayercan@users.noreply.github.com>
2022-06-07 20:22:25 +0300

    asd

--- 6-27Nisan/Siparis/Siparis/src/java/SiparisBean.java
+++ 6-27Nisan/Siparis/Siparis/src/java/SiparisBean.java
@@ -7,15 +7,15 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
 import java.sql.Connection;
- import java.sql.PreparedStatement;
- import java.sql.ResultSet;
- import java.sql.SQLException;
- import javax.annotation.Resource;
- import javax.faces.bean.ManagedBean;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.annotation.Resource;
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.event.ActionEvent;
- import javax.sql.DataSource;
- import javax.sql.rowset.CachedRowSet;
+import javax.sql.DataSource;
+import javax.sql.rowset.CachedRowSet;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -27,7 +27,7 @@ import javax.naming.NamingException;
 @ManagedBean ( name="siparisler" )
 @SessionScoped
 public class SiparisBean {
-private String bulunacak_siparis;
+    private String bulunacak_siparis;
 
     public void setBulunacak_siparis(String bulunacak_siparis) {
         this.bulunacak_siparis = bulunacak_siparis;
@@ -39,58 +39,59 @@ private String bulunacak_siparis;
 
 
 
-CachedRowSet rowSet=null;
+    CachedRowSet rowSet=null;
 
     DataSource dataSource;
-   public SiparisBean() {
+    public SiparisBean() {
         try {
-			Context ctx = new InitialContext();
-			dataSource = (DataSource)ctx.lookup("jdbc/sample");
-		} catch (NamingException e) {
-			e.printStackTrace();
-		}
+            Context ctx = new InitialContext();
+            dataSource = (DataSource)ctx.lookup("jdbc/sample");
+        } 
+        catch (NamingException e) {
+            e.printStackTrace();
+        }
     }
     
   
  
- public ResultSet bul() throws SQLException
- {
- // check whether dataSource was injected by the server
- if ( dataSource == null )
- throw new SQLException( "Unable to obtain DataSource" );
+    public ResultSet bul() throws SQLException
+    {
+        // check whether dataSource was injected by the server
+        if ( dataSource == null )
+            throw new SQLException( "Unable to obtain DataSource" );
 
- // obtain a connection from the connection pool
- Connection connection = dataSource.getConnection();
+        // obtain a connection from the connection pool
+        Connection connection = dataSource.getConnection();
 
- // check whether connection was successful
- if ( connection == null )
- throw new SQLException( "Unable to connect to DataSource" );
+        // check whether connection was successful
+        if ( connection == null )
+            throw new SQLException( "Unable to connect to DataSource" );
 
- try
- {
+        try
+        {
 
- PreparedStatement ps =
- connection.prepareStatement( "select order_num, customer.customer_id, name,description,quantity,sales_date " +
-" from PURCHASE_ORDER, customer, Product " +
-"where order_num=? and " +
-"PURCHASE_ORDER.CUSTOMER_ID=customer.CUSTOMER_ID and " +
-"PURCHASE_ORDER.PRODUCT_ID=product.PRODUCT_ID" );
- //SQL cümlesinin içinde değişken kullanmak istersek ? işaretini kullanırız.
- //bu işaret aynı SQL cümlesinde birkaç tane olabilir. Sıra numaralarına göre sırayla değer 
- // göndermemiz gerekir.
- // Bu örnekte bir tane ? işareti var.Dolayısıyla setInt (1, Integer.parseInt(getBulunacak_siparis())
- // yazarak birinci ? işaretine getBulunacak_siparis() değerini yolluyoruz. Bu değer web sayfasında 
- //kullanıcıdan gelmişti.
- ps.setInt(1, Integer.parseInt(getBulunacak_siparis()));
- rowSet = new com.sun.rowset.CachedRowSetImpl();
- rowSet.populate( ps.executeQuery() );
-return rowSet;
- } // end try
- finally
- {
- connection.close(); // return this connection to pool
- } // end finally
- } 
+            PreparedStatement ps =
+            connection.prepareStatement( "select order_num, customer.customer_id, name,description,quantity,sales_date " +
+            " from PURCHASE_ORDER, customer, Product " +
+            "where order_num=? and " +
+            "PURCHASE_ORDER.CUSTOMER_ID=customer.CUSTOMER_ID and " +
+            "PURCHASE_ORDER.PRODUCT_ID=product.PRODUCT_ID" );
+            //SQL cümlesinin içinde değişken kullanmak istersek ? işaretini kullanırız.
+            //bu işaret aynı SQL cümlesinde birkaç tane olabilir. Sıra numaralarına göre sırayla değer 
+            // göndermemiz gerekir.
+            // Bu örnekte bir tane ? işareti var.Dolayısıyla setInt (1, Integer.parseInt(getBulunacak_siparis())
+            // yazarak birinci ? işaretine getBulunacak_siparis() değerini yolluyoruz. Bu değer web sayfasında 
+            //kullanıcıdan gelmişti.
+            ps.setInt(1, Integer.parseInt(getBulunacak_siparis()));
+            rowSet = new com.sun.rowset.CachedRowSetImpl();
+            rowSet.populate( ps.executeQuery() );
+            return rowSet;
+        } // end try
+        finally
+        {
+            connection.close(); // return this connection to pool
+        } // end finally
+    } 
 
 
 
--- 7-11Mayis/DBProject/src/java/KitapBean.java
+++ 7-11Mayis/DBProject/src/java/KitapBean.java
@@ -7,15 +7,15 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
 import java.sql.Connection;
- import java.sql.PreparedStatement;
- import java.sql.ResultSet;
- import java.sql.SQLException;
- import javax.annotation.Resource;
- import javax.faces.bean.ManagedBean;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.annotation.Resource;
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.event.ActionEvent;
- import javax.sql.DataSource;
- import javax.sql.rowset.CachedRowSet;
+import javax.sql.DataSource;
+import javax.sql.rowset.CachedRowSet;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -27,14 +27,14 @@ import javax.naming.NamingException;
 @ManagedBean ( name="kitapBean" )
 @SessionScoped
 public class KitapBean {
-private String isbn;
-private String ad;
-private String yazar_id;
-private String basim_yili;
-private String yayin_evi;
-private String bulunacak_kitap;
+    private String isbn;
+    private String ad;
+    private String yazar_id;
+    private String basim_yili;
+    private String yayin_evi;
+    private String bulunacak_kitap;
 
-private String silinecek_isbn;
+    private String silinecek_isbn;
 
     public void setBulunacak_kitap(String bulunacak_kitap) {
         this.bulunacak_kitap = bulunacak_kitap;
@@ -43,196 +43,189 @@ private String silinecek_isbn;
     public String getBulunacak_kitap() {
         return bulunacak_kitap;
     }
-CachedRowSet rowSet=null;
-
-    /**
-     * Creates a new instance of KitapBean
-     */
-
-  DataSource dataSource;
-   public KitapBean() {
-        try {
-			Context ctx = new InitialContext();
-			dataSource = (DataSource)ctx.lookup("jdbc/sample");
-		} catch (NamingException e) {
-			e.printStackTrace();
-		}
-    }
     
-  public String getIsbn()
- {
- return isbn;
- } 
+    CachedRowSet rowSet=null;
 
- 
- public void setIsbn( String isbn )
- {
- this.isbn = isbn;
- } 
-   public String getAd()
- {
- return ad;
- } 
+        /**
+         * Creates a new instance of KitapBean
+         */
 
- 
- public void setAd( String ad )
- {
- this.ad = ad;
- } 
+    DataSource dataSource;
+    public KitapBean() {
+        try {
+            Context ctx = new InitialContext();
+            dataSource = (DataSource)ctx.lookup("jdbc/sample");
+        }
+        catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public String getIsbn()
+    {
+        return isbn;
+    } 
 
- public String getYazar_id()
- {
- return yazar_id;
- } 
+    public void setIsbn( String isbn )
+    {
+        this.isbn = isbn;
+    } 
+    
+    public String getAd()
+    {
+        return ad;
+    } 
 
- 
- public void setYazar_id( String yazar_id )
- {
- this.yazar_id = yazar_id;
- } 
+    public void setAd( String ad )
+    {
+        this.ad = ad;
+    } 
 
-public String getBasim_yili()
- {
- return basim_yili;
- } 
+    public String getYazar_id()
+    {
+        return yazar_id;
+    } 
 
- 
- public void setBasim_yili( String basim_yili )
- {
- this.basim_yili = basim_yili;
- } 
+    public void setYazar_id( String yazar_id )
+    {
+        this.yazar_id = yazar_id;
+    } 
 
- public String getYayin_evi()
- {
- return yayin_evi;
- } 
+    public String getBasim_yili()
+    {
+        return basim_yili;
+    } 
 
- 
- public void setYayin_evi( String yayin_evi )
- {
- this.yayin_evi = yayin_evi;
- }
+    public void setBasim_yili( String basim_yili )
+    {
+        this.basim_yili = basim_yili;
+    } 
 
-  
- 
- 
-  public String getSilinecek_isbn()
- {
- return silinecek_isbn;
- } 
+    public String getYayin_evi()
+    {
+        return yayin_evi;
+    } 
 
- 
- public void setSilinecek_isbn( String silinecek_isbn )
- {
- this.silinecek_isbn = silinecek_isbn;
- } 
- 
- public String ekle() throws SQLException
- {
- // check whether dataSource was injected by the server
- if ( dataSource == null )
- throw new SQLException( "Unable to obtain DataSource" );
+    public void setYayin_evi( String yayin_evi )
+    {
+        this.yayin_evi = yayin_evi;
+    }
 
- // obtain a connection from the connection pool
- Connection connection = dataSource.getConnection();
+    public String getSilinecek_isbn()
+    {
+        return silinecek_isbn;
+    } 
 
- // check whether connection was successful
- if ( connection == null )
- throw new SQLException( "Unable to connect to DataSource" );
+    public void setSilinecek_isbn( String silinecek_isbn )
+    {
+        this.silinecek_isbn = silinecek_isbn;
+    } 
 
- try
- {
- // create a PreparedStatement to insert a new book entry
- PreparedStatement addEntry =
- connection.prepareStatement( "INSERT INTO KITAPLAR" +
- "(ISBN,ad,yazar_id,basim_yili,yayin_evi)" +
- "VALUES ( ?, ?, ?, ?,? )" );
+    public String ekle() throws SQLException
+    {
+        // check whether dataSource was injected by the server
+        if ( dataSource == null )
+            throw new SQLException( "Unable to obtain DataSource" );
 
- // specify the PreparedStatement's arguments
- addEntry.setInt( 1, Integer.parseInt(getIsbn()) );
- addEntry.setString( 2, getAd() );
- addEntry.setInt( 3, Integer.parseInt(getYazar_id()) );
- addEntry.setInt( 4, Integer.parseInt(getBasim_yili()) );
- addEntry.setString( 5, getYayin_evi() );
+        // obtain a connection from the connection pool
+        Connection connection = dataSource.getConnection();
 
+        // check whether connection was successful
+        if ( connection == null )
+            throw new SQLException( "Unable to connect to DataSource" );
 
-addEntry.executeUpdate(); // insert the entry
- return "index"; // go back to index.xhtml home page
- } // end try
- finally
- {
- connection.close(); // return this connection to pool
- } // end finally
- } 
+        try
+        {
+            // create a PreparedStatement to insert a new book entry
+            PreparedStatement addEntry =
+            connection.prepareStatement( "INSERT INTO KITAPLAR" +
+            "(ISBN,ad,yazar_id,basim_yili,yayin_evi)" +
+            "VALUES ( ?, ?, ?, ?,? )" );
+
+            // specify the PreparedStatement's arguments
+            addEntry.setInt( 1, Integer.parseInt(getIsbn()) );
+            addEntry.setString( 2, getAd() );
+            addEntry.setInt( 3, Integer.parseInt(getYazar_id()) );
+            addEntry.setInt( 4, Integer.parseInt(getBasim_yili()) );
+            addEntry.setString( 5, getYayin_evi() );
 
 
- public ResultSet bul() throws SQLException
- {
- // check whether dataSource was injected by the server
- if ( dataSource == null )
- throw new SQLException( "Unable to obtain DataSource" );
-
- // obtain a connection from the connection pool
- Connection connection = dataSource.getConnection();
-
- // check whether connection was successful
- if ( connection == null )
- throw new SQLException( "Unable to connect to DataSource" );
-
- try
- {
- // create a PreparedStatement to insert a new address book entry
- PreparedStatement ps =
- connection.prepareStatement( "select kitaplar.ISBN,kitaplar.ad,yazarlar.ad as yazar_ad,yazarlar.soyad as yazar_soyad,"
-         + "yazarlar.id,kitaplar.basim_yili, kitaplar.yayin_evi from kitaplar,yazarlar " +
-         "where kitaplar.ad=? and kitaplar.yazar_id=yazarlar.id" );
- ps.setString( 1, getBulunacak_kitap() );
- rowSet = new com.sun.rowset.CachedRowSetImpl();
- rowSet.populate( ps.executeQuery() );
-return rowSet;
- } // end try
- finally
- {
- connection.close(); // return this connection to pool
- } // end finally
- } 
+            addEntry.executeUpdate(); // insert the entry
+            return "index"; // go back to index.xhtml home page
+        } // end try
+        finally
+        {
+            connection.close(); // return this connection to pool
+        } // end finally
+    } 
 
 
+    public ResultSet bul() throws SQLException
+    {
+        // check whether dataSource was injected by the server
+        if ( dataSource == null )
+            throw new SQLException( "Unable to obtain DataSource" );
 
- 
- 
- public String sil() throws SQLException
- {
- // check whether dataSource was injected by the server
- if ( dataSource == null ){ 
-      throw new SQLException( "Unable to obtain DataSource" );
- }
+        // obtain a connection from the connection pool
+        Connection connection = dataSource.getConnection();
 
- // obtain a connection from the connection pool
- Connection connection = dataSource.getConnection();
+        // check whether connection was successful
+        if ( connection == null )
+            throw new SQLException( "Unable to connect to DataSource" );
 
- // check whether connection was successful
- if ( connection == null )
- throw new SQLException( "Unable to connect to DataSource" );
+        try
+        {
+        // create a PreparedStatement to insert a new address book entry
+            PreparedStatement ps =
+            connection.prepareStatement( "select kitaplar.ISBN,kitaplar.ad,yazarlar.ad as yazar_ad,yazarlar.soyad as yazar_soyad,"
+                + "yazarlar.id,kitaplar.basim_yili, kitaplar.yayin_evi from kitaplar,yazarlar " +
+                "where kitaplar.ad=? and kitaplar.yazar_id=yazarlar.id" );
+            ps.setString( 1, getBulunacak_kitap() );
+            rowSet = new com.sun.rowset.CachedRowSetImpl();
+            rowSet.populate( ps.executeQuery() );
+            return rowSet;
+        } // end try
+        finally
+        {
+            connection.close(); // return this connection to pool
+        } // end finally
+    } 
 
- try
- {
- // create a PreparedStatement to insert a new address book entry
- PreparedStatement myObject =
- connection.prepareStatement( "delete from kitaplar where isbn=?");
 
- // specify the PreparedStatement's arguments
- myObject.setInt( 1, Integer.parseInt(getSilinecek_isbn()) );
- 
-myObject.executeUpdate(); // insert the entry
- return "index"; // go back to index.xhtml page
- } // end try
- finally
- {
- connection.close(); // return this connection to pool
- } // end finally
- } 
+
+
+
+    public String sil() throws SQLException
+    {
+        // check whether dataSource was injected by the server
+        if ( dataSource == null ){ 
+             throw new SQLException( "Unable to obtain DataSource" );
+        }
+
+        // obtain a connection from the connection pool
+        Connection connection = dataSource.getConnection();
+
+        // check whether connection was successful
+        if ( connection == null )
+            throw new SQLException( "Unable to connect to DataSource" );
+
+        try
+        {
+            // create a PreparedStatement to insert a new address book entry
+            PreparedStatement myObject =
+            connection.prepareStatement( "delete from kitaplar where isbn=?");
+
+            // specify the PreparedStatement's arguments
+            myObject.setInt( 1, Integer.parseInt(getSilinecek_isbn()) );
+
+            myObject.executeUpdate(); // insert the entry
+            return "index"; // go back to index.xhtml page
+        } // end try
+        finally
+        {
+            connection.close(); // return this connection to pool
+        } // end finally
+    } 
 
 
  
